56a9d0931510f254f8afef94d7cfdbe6945e8e00
Tamashevich <tamashevichsg@qws-tsg-mac.local>
2017-12-22 16:21:01 +0300

    add scroll

--- src/app/giphy.service.ts
+++ src/app/giphy.service.ts
@@ -6,20 +6,22 @@ export class GiphyService {
   private giphyKey = 'dc6zaTOxFJmzC';
   private limit = 20;
   private oldRequest: string;
+  private offset = 0;
 
   constructor(private http: HttpClient) { }
 
   getGifs(request) {
-
-
+    if (this.oldRequest !== request) {
+      this.offset = 0;
+    }
     this.oldRequest = request;
-    const url = 'http://api.giphy.com/v1/gifs/search?q=' + this.oldRequest + '&api_key=' + this.giphyKey + '&limit=' + this.limit;
+    const url = 'http://api.giphy.com/v1/gifs/search?q=' + this.oldRequest + '&api_key=' + this.giphyKey + '&limit=' + this.limit + '&offset=' + this.offset;
     return this.http.get(url)
                     .toPromise();
   }
 
   getMoreGifs() {
-    this.limit += 20;
+    this.offset += 20;
     return this.getGifs(this.oldRequest);
   }
 
